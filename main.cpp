29e5885779c315adc48b5d3f50a5185c27c9c4a9
Gfauser99 <s205446@student.dtu.dk>
2023-04-23 16:46:11 +0200

    Print Empty board with last command

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+void printEmptyBoard();
+
+int Game = 1;
+char last_command[] = "LD";
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    while(last_command!="Q") {
+    printEmptyBoard();
+    scanf("%s",&last_command);
+
+
+    }
     return 0;
+}
+
+
+void printEmptyBoard(){
+    printf("C1 \tC2 \tC3 \tC4 \tC5 \tC6 \tC7 \t\n\n" );
+    printf("\t\t\t\t\t\t\t[]\tF1\n\t\t\t\t\t\t\t[]\tF2\n\t\t\t\t\t\t\t[]\tF3\n\t\t\t\t\t\t\t[]\tF4\n");
+    printf("LAST COMMAND: %s \nMessage: %s\nINPUT > ",last_command);
+
+
 }
