b22316bbd0a66f9523faf5aca76c396ffe1fe9e3
Gfauser99 <s205446@student.dtu.dk>
2023-04-25 10:40:16 +0200

    SW only works if file is loaded. File only loads upon LD

--- main.cpp
+++ main.cpp
@@ -8,9 +8,9 @@
 #define MAX_CARD_LENGTH 5
 void printEmptyBoard();
 void printLoadedBoard();
-
+int loadFile(char[]);
 int Game = 1;
-
+bool loaded = false;
 
 
 
@@ -21,8 +21,8 @@ char last_command[4] = "";
 char msg[]="";
 int main() {
 
-    char test123[]="KS";
-    FILE *file = fopen("cards.txt", "r");
+  //  char test123[]="KS";
+   /* FILE *file = fopen("cards.txt", "r");
     if (file == NULL) {
         printf("Error opening file.\n");
         return 1;
@@ -39,17 +39,17 @@ int main() {
         }
 
         i++;
-    }
+    }*/
 
-    fclose(file);
+   // fclose(file);
 
     // Print the array to check the result
-    for (int j = 0; j < i; j++) {
-        printf("%s\n", cards[j]);
-    }
-    printf("%s",cards[51]);
-    int result = strcmp(test123,cards[51]);
-    printf("%d\n",result);
+  //  for (int j = 0; j < i; j++) {
+  //      printf("%s\n", cards[j]);
+ //   }
+   // printf("%s",cards[51]);
+  //  int result = strcmp(test123,cards[51]);
+   // printf("%d\n",result);
     while(1) {
         printEmptyBoard();
         scanf("%s",last_command);
@@ -76,13 +76,16 @@ void printEmptyBoard(){
     int j = 1;
     int k = 0;
         //Print top of cards always
-    printf("C1 \tC2 \tC3 \tC4 \tC5 \tC6 \tC7 \t\n\n" );
+    printf("\nC1 \tC2 \tC3 \tC4 \tC5 \tC6 \tC7 \t\n\n" );
 
     if (strcmp(last_command,"")==0){
             printf("\t\t\t\t\t\t\t[]\tF1\n\n\t\t\t\t\t\t\t[]\tF2\n\n\t\t\t\t\t\t\t[]\tF3\n\n\t\t\t\t\t\t\t[]\tF4\n");
     }
 
     if (strcmp(last_command,"LD")==0){
+        loadFile("cards.txt");
+
+
         for (int i = 1; i < 9; i++) {
             if (i % 2 != 0) {
                 printf("[]\t[]\t[]\t[]\t[]\t[]\t[]\t\t[]\tF%d\n", j);
@@ -91,21 +94,14 @@ void printEmptyBoard(){
                 printf("[]\t[]\t[]\t[]\t[]\t[]\t[]\n");
             }
         }
-        printf("%s\t%s\t%s\n", cards[49], cards[50], cards[51]);
-        printf("[]\t[]\t[]\t[]\t[]\t[]\t[]\t\t[]\tF1\n"
-               "[]\t[]\t[]\t[]\t[]\t[]\t[]\n"
-               "[]\t[]\t[]\t[]\t[]\t[]\t[]\t\t[]\tF2\n"
-               "[]\t[]\t[]\t[]\t[]\t[]\t[]\n"
-               "[]\t[]\t[]\t[]\t[]\t[]\t[]\t\t[]\tF3\n"
-               "[]\t[]\t[]\t[]\t[]\t[]\t[]\n"
-               "[]\t[]\t[]\t[]\t[]\t[]\t[]\t\t[]\tF4\n"
-               "[]\t[]\t[]\n"
-               );
+        printf("[]\t[]\t[]\n");
+
     }
 
     if (strcmp(last_command, "SW") == 0) {
+        if (!loaded){ printf("No file loaded yet");}
         // Change k to 0 to start from the first index
-        for (int i = 1; i < 9; i++) {
+        else {for (int i = 1; i < 9; i++) {
             if (i % 2 != 0) {
                 printf("%s\t%s\t%s\t%s\t%s\t%s\t%s\t\t[]\tF%d\n", cards[k], cards[k+1], cards[k+2], cards[k+3], cards[k+4], cards[k+5], cards[k+6], j);
                 k = k + 7;
@@ -117,7 +113,7 @@ void printEmptyBoard(){
             }
         }
         printf("%s\t%s\t%s\n", cards[49], cards[50], cards[51]); // Change the last line to print the last 3 elements
-    }
+    }}
 
     if (strcmp(last_command, "SI") == 0) {
         char pile1[26][MAX_CARD_LENGTH], pile2[26][MAX_CARD_LENGTH];
@@ -136,14 +132,34 @@ void printEmptyBoard(){
 
 
 
-    printf("LAST COMMAND: %s \nMessage: %s\nINPUT > ", last_command,msg);
+    printf("\nLAST COMMAND: %s \nMessage: %s\nINPUT >", last_command,msg);
+
 
 
 }
 
-void printLoadedBoard(char *last_command, char cards[MAX_CARDS][MAX_CARD_LENGTH]){
-    printf("C1 \tC2 \tC3 \tC4 \tC5 \tC6 \tC7 \t\n\n" );
-    printf("\t\t\t\t\t\t\t[]\tF1\n\t\t\t\t\t\t\t[]\tF2\n\t\t\t\t\t\t\t[]\tF3\n\t\t\t\t\t\t\t[]\tF4\n");
-    printf("LAST COMMAND: %s \nMessage: %s\nINPUT > ",last_command,msg);
-}
+int loadFile(char cardfile[]) {
 
+    FILE *file = fopen(cardfile, "r");
+   // printf("%s",cardfile);
+    if (file == NULL) {
+        printf("Error opening file.\n");
+        return 1;
+    }
+
+
+    int i = 0;
+
+    while (fgets(cards[i], MAX_CARD_LENGTH, file) != NULL && i < MAX_CARDS) {
+// Remove newline character if present
+        size_t len = strlen(cards[i]);
+        if (cards[i][len - 1] == '\n') {
+            cards[i][len - 1] = '\0';
+        }
+
+        i++;
+    }
+    fclose(file);
+    loaded= true;
+    return 0;
+}
