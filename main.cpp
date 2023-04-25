3b7a7ea1d0580fc559ae2359b3d2f45edfba77d6
Gfauser99 <s205446@student.dtu.dk>
2023-04-25 10:50:16 +0200

    error msg when SL or SW with no file

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ bool loaded = false;
 
 char cards[MAX_CARDS][MAX_CARD_LENGTH];
 char last_command[4] = "";
-char msg[]="";
+
 int main() {
 
   //  char test123[]="KS";
@@ -73,19 +73,17 @@ int main() {
 
 
 void printEmptyBoard(){
+    char msg[30]="";
     int j = 1;
     int k = 0;
         //Print top of cards always
     printf("\nC1 \tC2 \tC3 \tC4 \tC5 \tC6 \tC7 \t\n\n" );
 
-    if (strcmp(last_command,"")==0){
-            printf("\t\t\t\t\t\t\t[]\tF1\n\n\t\t\t\t\t\t\t[]\tF2\n\n\t\t\t\t\t\t\t[]\tF3\n\n\t\t\t\t\t\t\t[]\tF4\n");
-    }
+
 
     if (strcmp(last_command,"LD")==0){
+
         loadFile("cards.txt");
-
-
         for (int i = 1; i < 9; i++) {
             if (i % 2 != 0) {
                 printf("[]\t[]\t[]\t[]\t[]\t[]\t[]\t\t[]\tF%d\n", j);
@@ -95,11 +93,11 @@ void printEmptyBoard(){
             }
         }
         printf("[]\t[]\t[]\n");
-
     }
 
     if (strcmp(last_command, "SW") == 0) {
-        if (!loaded){ printf("No file loaded yet");}
+        if (!loaded){
+             strcpy(msg,"Error - no file loaded yet");}
         // Change k to 0 to start from the first index
         else {for (int i = 1; i < 9; i++) {
             if (i % 2 != 0) {
@@ -116,6 +114,8 @@ void printEmptyBoard(){
     }}
 
     if (strcmp(last_command, "SI") == 0) {
+        if (!loaded){
+            strcpy(msg,"Error - no file loaded yet");}
         char pile1[26][MAX_CARD_LENGTH], pile2[26][MAX_CARD_LENGTH];
         for (int i=0;i<26;i++){
         strcpy(pile1[i], cards[i]);
@@ -130,7 +130,9 @@ void printEmptyBoard(){
 
     }
 
-
+    if (strcmp(last_command,"")==0 || strcmp(msg,"")!=0){
+        printf("\t\t\t\t\t\t\t[]\tF1\n\n\t\t\t\t\t\t\t[]\tF2\n\n\t\t\t\t\t\t\t[]\tF3\n\n\t\t\t\t\t\t\t[]\tF4\n");
+    }
 
     printf("\nLAST COMMAND: %s \nMessage: %s\nINPUT >", last_command,msg);
 
